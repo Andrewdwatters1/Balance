c390fb2fe12c8c6375e45fd872196ec2ab400562
tmonty312 <tmonty312@gmail.com>
2018-09-19 10:42:54 -0600

    pull login page from master

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import store from './redux/store'
-// import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>
-, document.getElementById('root'));
-=======
 import {Provider} from 'react-redux';
 import store from './redux/store';
 // import registerServiceWorker from './registerServiceWorker';
@@ -22,5 +11,4 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.getElementById('root'));
->>>>>>> master
 // registerServiceWorker();
